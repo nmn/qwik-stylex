9409d18233f143517df0c1fb827a1001526141fe
PatrickJS <github@patrickjs.com>
2023-12-08 23:17:23 -0800

    fix:

--- src/root.tsx
+++ src/root.tsx
@@ -7,6 +7,7 @@ import {
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
+import "virtual:stylex.css";
 
 export default component$(() => {
   /**
--- vite.config.mjs
+++ vite.config.mjs
@@ -3,10 +3,11 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import styleX from "./vite-stylex-plugin/index.mjs";
+import Inspect from "vite-plugin-inspect";
 
 export default defineConfig(() => {
   return {
-    plugins: [styleX(), qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [Inspect(), styleX(), qwikCity(), qwikVite(), tsconfigPaths()],
     dev: { headers: { "Cache-Control": "public, max-age=0" } },
     preview: { headers: { "Cache-Control": "public, max-age=600" } },
   };
